49a3e5bc72f56fdd25d65289747caeb54fa912ed
osawa-koki <github@osawa.dev>
2023-03-06 00:26:48 +0900

    PrettierをESLintに統合。

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,18 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: 'standard-with-typescript',
-  overrides: [
+  extends: [
+    'standard-with-typescript',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/standard',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
+  plugins: ['prettier'],
   rules: {
+    'prettier/prettier': 'error'
   }
-}
+};
