590b084bb3a8ea9c2f4b4bd4c83bd4d9aaa2e412
osawa-koki <github@osawa.dev>
2023-03-06 01:10:43 +0900

    不要なコード消した。

--- jest.config.ts
+++ jest.config.ts
@@ -11,7 +11,6 @@ export default {
   // bail: 0,
 
   // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "C:\\Users\\kokof\\AppData\\Local\\Temp\\jest",
 
   // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
