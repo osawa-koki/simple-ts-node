a6d94c56637862ede6940c3e1e2a01b88b1172be
osawa-koki <github@osawa.dev>
2023-03-06 00:34:44 +0900

    小さな修正。

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,18 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['standard-with-typescript', 'plugin:prettier/recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint'
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: 12,
     sourceType: 'module'
   },
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
     // additional rules
   }
